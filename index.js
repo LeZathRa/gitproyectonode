7b6628c506bc6b834dadc1b82a935235545f11df
Lessiel Ramirez <lessiel.rh@gmail.com>
2024-04-02 11:34:59 -0500

    tarea

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const app = express();
 
-const productos = [
+// Middleware para analizar el cuerpo de la solicitud como JSON
+app.use(express.json());
+
+let productos = [
     { id: 1, nombre: 'Producto 1', precio: 10 },
     { id: 2, nombre: 'Producto 2', precio: 20 },
     { id: 3, nombre: 'Producto 3', precio: 30 }
-  ];
+];
 
 app.get('/productos', (req, res) => {
-res.json(productos);
+    res.json(productos);
+});
+
+app.post('/productos', (req, res) => {
+    const nuevoProducto = req.body;
+    productos.push(nuevoProducto);
+    res.status(201).json(nuevoProducto);
+});
+
+app.put('/productos/:id', (req, res) => {
+    const idProducto = req.params.id;
+    const productoActualizado = req.body;
+    productos = productos.map(producto => {
+        if (producto.id.toString() === idProducto) {
+            return { ...producto, ...productoActualizado };
+        }
+        return producto;
+    });
+    res.json(productos);
+});
+
+app.delete('/productos/:id', (req, res) => {
+    const idProducto = req.params.id;
+    productos = productos.filter(producto => producto.id.toString() !== idProducto);
+    res.json(productos);
 });
 
 const port = process.env.PORT || 3000;
